77fa9d50f9d17259dbed3cc412f28922df180f2c
Aarti Biradar <aartibiradar21@navgurukul.org>
2024-09-25 16:31:28 +0530

    rename files

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-// const result = document.getElementById("result");
-// const btn = document.getElementById("search-btn");
-
-
-// btn.addEventListener("click", () => {
-//    let input = document.getElementById("input").value;
-//    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`)
-//        .then((response) => response.json())
-//        .then((data) => {
-//            console.log(data);
-
-
-//            const word = data[0].word;
-//            const partOfSpeech = data[0].meanings[0].partOfSpeech;
-//            const definition = data[0].meanings[0].definitions[0].definition;
-//            const example = data[0].meanings[0].definitions[0].example || "No example available";
-
-
-//            result.innerHTML = `
-//                <div class="result">
-//                    <p>Word: - ${word}</p>
-//                    <p>Part of Speech: - ${partOfSpeech}</p>
-//                    <p>Definition: - ${definition}</p>
-//                    <p>Example: ${example}</p>
-//                </div>`;
-//        })
-//        .catch((error) => {
-//            console.error('Error fetching data:', error);
-//            result.innerHTML = '<p>Word not found. Please try again.</p>';
-//        });
-// });
-
 const result = document.getElementById("result");
 const btn = document.getElementById("search-btn");
 
@@ -42,7 +10,6 @@ btn.addEventListener("click", () => {
     }
     result.innerHTML = '<p>Loading...</p>';
 
-    // fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`)
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`)
 
         .then((response) => {
